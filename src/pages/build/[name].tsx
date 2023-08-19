77d3e2de033e3376847e819d592fc9cdbf668c8f
FahimMontasir <fahimmontasirshakil@gmail.com>
2023-08-20 00:34:03 +0600

    [add] product category page

---
+++ src/pages/build/[name].tsx
@@ -0,0 +1,26 @@
+import ProductCard from "@/components/common/ProductCard";
+import { Product } from "@/interface";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+
+export const getServerSideProps: GetServerSideProps<{
+  products: Product[];
+}> = async ({ params }) => {
+  const res = await fetch(
+    `http://localhost:3000/api/product/category/${params?.name}`
+  );
+  const data = await res.json();
+
+  return { props: { products: data.products } };
+};
+
+export default function BuildCategoryPage({
+  products,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <section className="flex flex-wrap gap-3 justify-center">
+      {products.map((product) => (
+        <ProductCard product={product} key={product._id} isBuild />
+      ))}
+    </section>
+  );
+}
