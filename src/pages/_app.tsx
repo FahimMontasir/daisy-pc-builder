2ccba0093a41ba5a4dad401753ad8010e10f1112
FahimMontasir <fahimmontasirshakil@gmail.com>
2023-08-20 00:30:45 +0600

    [init] setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import store from "@/lib/redux";
+import RootLayout from "@/components/layout";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Provider store={store}>
+        <RootLayout>
+          <Component {...pageProps} />
+        </RootLayout>
+      </Provider>
+    </SessionProvider>
+  );
 }
